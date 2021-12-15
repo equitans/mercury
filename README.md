e71155a17857e1a7a212edef35add6fd41287b16
equitans <equitans@protonmail.com>
2021-12-15 16:46:17 +0300

    ...

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mercury
 p2p or tcp/ip chat.
+
+Compile GCC:
+<code>gcc main.c -lws2_32</code>
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+//gcc main.c -lws2_32
